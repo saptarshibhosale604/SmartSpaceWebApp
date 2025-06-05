33df57d5ef7527389d68dbebe27bbeb0ac95256b
BhosaleS <Saptarshisuresh.Bhosale@hmhco.com>
2025-06-05 14:57:54 +0530

    Temp

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let button01 = document.querySelector('.button01')
 let current = "off"
 button01.addEventListener('click', function () {
     if (current == "off") {
-        button01.style.backgroundImage = "url('./IconFan05.png')"
+        button01.style.backgroundImage = "url('./IconFan05On.png')"
         current = "on"
     } else {
-        button01.style.backgroundImage = "url('./IconTube05.png')"
+        button01.style.backgroundImage = "url('./IconFan05Off.png')"
         current = "off"
     }
 
@@ -15,7 +15,7 @@ button01.addEventListener('click', function () {
     const loadStatus = button01.getAttribute("data-load-status");
 
     const url = "https://mhw4tdxdjyofllxw7yyjnvxytq0sbftl.lambda-url.ap-south-1.on.aws/?load_num=${loadNum}&load_status=${loadStatus}";
-
+    // https://mhw4tdxdjyofllxw7yyjnvxytq0sbftl.lambda-url.ap-south-1.on.aws/?load_num=1&load_status=0";
     fetch(url, {
         headers: {
             "Accept": "application/json"
